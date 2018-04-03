0ebe3f8a5bd98a4ad031f9f4a6b1213ba85db5a5
duyboy135 <duyboy135@gmail.com>
2018-04-03 13:21:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
   	io.emit('chat message', msg);
     console.log('message: ' + msg);
-    alert("Hi! I am duyboy135");
   });
 });
 
